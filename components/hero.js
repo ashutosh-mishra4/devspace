7092b8402a3028a76c356c95bc98a9fd8656ad14
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-10 23:13:13 +0530

    hero section ui done

--- components/hero.js
+++ components/hero.js
@@ -3,13 +3,25 @@ import Image from "next/image";
 
 function hero() {
   return (
-    <section className="bg-sky-700">
-      <h1 className="text-4xl font-bold text-center mx-4">
-        Find your next{" "}
-        <span className="underline decoration-pink-500">tech space</span>
+    <section className="bg-background">
+    <div className="bg-green-300 border-b-2 border-t-2 border-black">
+      <h1 className="text-4xl font-bold text-center px-16 pt-10 font-cursive text-wheat">
+        DevSpaces
       </h1>
-      <p className="text-center text-lg mx-4">Get reminded for your favorite Twitter spaces on tech, programming, and development in 3 simple steps</p>
-      {/* <Image src="/Technologies.svg" height={1867} width={1761} /> */}
+      <div className="max-w-full flex justify-center">
+        <Image
+          className="mx-auto"
+          src="/image.png"
+          // layout="responsive"
+          width={300}
+          height={300}
+        />
+      </div>
+      <p className="text-center text-xl text-white px-10 font-play">Find and save upcoming Twitter spaces on tech, programming, and development</p>
+      <div className="flex justify-center">
+        <button className="bg-wheat font-play font-black text-lg px-24 py-4 my-5 rounded">Get Started</button>
+      </div>
+    </div>
     </section>
   );
 }
