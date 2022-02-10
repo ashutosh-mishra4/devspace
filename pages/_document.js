6c02613e12487465bd7626143a190f40a0dad584
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-10 23:12:55 +0530

    google fonts import

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Play&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Monoton&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
