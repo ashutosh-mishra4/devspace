117fae479ff6afe0a010ff7ccbbaba5d852d2cff
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-23 00:12:12 +0530

    added comments

--- pages/web.js
+++ pages/web.js
@@ -2,6 +2,7 @@ import React from "react"
 
 export async function getServerSideProps(context) {
 
+    // fetching relevant data from Twitter Spaces API related to query 'web'
     const token = process.env.TWITTER_BEARER_TOKEN
     const endpoint = "https://api.twitter.com/2/spaces/search?query=web&state=all&space.fields=creator_id,participant_count,title,scheduled_start&user.fields=name,id,username,url,profile_image_url"
     const res = await fetch(endpoint, {
@@ -24,16 +25,17 @@ export async function getServerSideProps(context) {
 function tech({ datas }) {
     return (
         <div className="flex flex-col gap-6 justify-center bg-dark-background items-center">
-            <p>hello woel</p>
+            {/* Section for displaying all the spaces with 'web' keyword */}
             {datas.data.map((item, key) => {
                 return (
                     <div key={key} className="bg-background rounded-xl w-5/6 h-auto p-6">
-                        {/* <p className="text-white">{item.state.toUppercase()}</p> */}
                         <p className="text-white font-bold text-3xl whitespace-normal">{item.title}</p>
+                        {/* Show participants if the space is live else show starting time */}
                         {item.state=='live' ?
                             <div>
                                 <p className="text-white pt-2">{item.participant_count} people are listening</p>
                                 <button className="bg-wheat p-2 mt-2 font-bold rounded">
+                                    {/* Link to the space */}
                                     <a href={`https://twitter.com/i/spaces/${item.id}`}>
                                         Start Listening
                                     </a>    
@@ -42,6 +44,7 @@ function tech({ datas }) {
                             <div>
                                 <p className="text-white pt-2">Starting at {item.scheduled_start}</p>
                                 <button className="bg-wheat mt-2 p-2 font-bold rounded">
+                                    {/* Link to the space */}
                                     <a href={`https://twitter.com/i/spaces/${item.id}`}>
                                         Set Reminder
                                     </a>
@@ -51,7 +54,6 @@ function tech({ datas }) {
                     </div>
                 )
             })}
-            {console.log(datas.data.map(item => item.title))}
         </div>    
     )
 }
