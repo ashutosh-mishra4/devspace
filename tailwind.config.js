7d67bcb3b35527bf0d69377e3d701e44e83d2e9d
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-22 23:56:32 +0530

    added dark background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     colors: {
       'background': '#1F1D36',
       'white': '#ffffff',
-      'wheat': '#f5deb3'
+      'wheat': '#f5deb3',
+      'dark-background': '#18191a'
     },
     extend: {
       fontFamily: {
