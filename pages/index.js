1dde9ae6f737230d9a701eb5a83423f65e6bd9dc
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-23 00:13:15 +0530

    removed unnecessary things

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Header from '../components/header'
 import Hero from '../components/hero'
 
-export default function Home() {
+export default function Home({datas}) {
   return (
     <div>
       {/* <Header /> */}
