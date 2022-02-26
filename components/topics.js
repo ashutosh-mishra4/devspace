4fd69d158ff82588af88698927ea2ff65bec0145
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-26 23:53:21 +0530

    mobile version finished

--- components/topics.js
+++ components/topics.js
@@ -1,34 +1,43 @@
-
-
+// Component to display all the spaces related keywords card
 function topics() {
     return (
         <div className="flex bg-white flex-col gap-6 justify-center font-play items-center">
         <p className="text-3xl mt-10 font-black">Select your Keyword</p>
+        
+        {/*Card for Tech Keyword*/}
         <div className="font-play bg-dark-background border-2 rounded-2xl">
             <img src="/tech.jpg" className="w-80 h-48 rounded-2xl object-cover"/>
             <p className="text-white text-4xl mt-4 px-4 font-black">Tech</p>
-            <p className="text-white w-80 mt-4 px-4">This page will show all the live and scheduled Twitter spaces with 'Web' keyword</p>
+            <p className="text-white w-80 mt-4 px-4">This page will show all the live and scheduled Twitter spaces with 'Tech' keyword</p>
             <button className="bg-wheat font-play font-black text-xl m-4 px-24 py-4 my-5 rounded">Visit Now</button>
         </div>
+
+        {/*Card for Web Keyword*/}
         <div className="font-play bg-dark-background rounded-2xl border-2">
             <img src="/web.jpg" className="w-80 h-48 rounded-2xl object-cover"/>
             <p className="text-white text-4xl mt-4 px-4 font-black">Web</p>
             <p className="text-white w-80 mt-4 px-4">This page will show all the live and scheduled Twitter spaces with 'Web' keyword</p>
             <button className="bg-wheat font-play font-black text-xl m-4 px-24 py-4 my-5 rounded">Visit Now</button>
         </div>
+
+        {/*Card for Dev Keyword*/}
         <div className="font-play bg-dark-background rounded-2xl border-2">
             <img src="/dev.jpg" className="w-80 h-48 rounded-2xl object-cover"/>
             <p className="text-white text-4xl mt-4 px-4 font-black">Dev</p>
             <p className="text-white w-80 mt-4 px-4">This page will show all the live and scheduled Twitter spaces with 'Dev' keyword</p>
             <button className="bg-wheat font-play font-black text-xl m-4 px-24 py-4 my-5 rounded">Visit Now</button>
         </div>
+
+        {/*Card for Developer Keyword*/}
         <div className="font-play bg-dark-background rounded-2xl border-2">
             <img src="/developer.jpg" className="w-80 h-48 rounded-2xl object-cover"/>
             <p className="text-white text-4xl mt-4 px-4 font-black ">Developer</p>
             <p className="text-white w-80 mt-4 px-4">This page will show all the live and scheduled Twitter spaces with 'Developer' keyword</p>
             <button className="bg-wheat font-play font-black text-xl m-4 px-24 py-4 my-5 rounded">Visit Now</button>
         </div>
-        <div className="font-play bg-dark-background rounded-2xl border-2">
+
+        {/*Card for Programming Keyword*/}
+        <div className="font-play bg-dark-background rounded-2xl border-2 mb-10">
             <img src="/programming.jpg" className="w-80 h-48 rounded-2xl object-cover"/>
             <p className="text-white text-4xl mt-4 px-4 font-black">Programming</p>
             <p className="text-white w-80 mt-4 px-4">This page will show all the live and scheduled Twitter spaces with 'Programming' keyword</p>
--- components/working.js
+++ components/working.js
@@ -1,15 +1,20 @@
 function HowItWorks() {
     return (
-        <div className="flex bg-orange flex-col bg-wheat text-center border-b-2 border-black gap-8 justify-center items-center">
-            <p className="text-3xl text-center font-black font-play mt-10">How it Works</p>
-            <div className="bg-white border-2 w-80 h-10 flex items-center justify-center rounded">
-                <p className="font-play text-xl">1. Sign up to the app</p>
-            </div>
-            <div className="bg-white border-2 p-10 w-80 h-10 flex items-center justify-center rounded">
-                <p className="font-play text-xl">2. Select a keyword from <a>Keywords</a></p>
-            </div>
-            <div className="bg-white border-2 mb-10 w-80 h-10 flex items-center justify-center rounded">
-                <p className="font-play text-xl">3. Enjoy your favorite spaces</p>
+        <div className="bg-orange">
+            <p className="text-3xl py-8 text-center font-black font-play">How it Works</p>
+            <div className="flex font-play flex-col text-center md:flex-row border-b-2 border-black gap-8 justify-center items-center">
+                <div className="bg-white border-2 w-80 px-14 h-48 flex flex-col items-center justify-center rounded">
+                    <p className="font-black text-2xl py-2">1. Sign up</p>
+                    <p>Sign up to the app with your socials to checkout our library of tech spaces </p>
+                </div>
+                <div className="bg-white border-2 p-10 w-80 h-48 flex flex-col items-center justify-center rounded">
+                    <p className="font-black text-2xl py-2">2. Select a keyword</p>
+                    <p className="text-md">Select a keyword from <a>keywords</a> to see the current live and scheduled spaces of that keyword</p>
+                </div>
+                <div className="bg-white border-2 mb-10 w-80 px-14 h-48 flex flex-col items-center justify-center rounded">
+                    <p className="font-black text-2xl py-2">3. Enjoy</p>
+                    <p>Get access to the awesome live and scheduled spaces. Join it live or set your reminder</p>
+                </div>
             </div>
         </div>
     )
