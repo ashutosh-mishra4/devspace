0d0c0218bff308eaf23096401af9291f2121efde
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-26 23:52:42 +0530

    footer created

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+function footer() {
+    return (
+        <div className="text-white text-center font-play mt-10">
+            Made with Love by Ashutosh Mishra
+        </div>
+    )
+}
+
+export default footer
