48999a87029a4bb605dcd5625b264adcfc8a234b
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-27 21:14:42 +0530

    minor edits

--- components/hero.js
+++ components/hero.js
@@ -29,12 +29,12 @@ function hero() {
     {/* Hero Section for tablets and other big screens */}
     <div className="hidden md:flex flex-row">
       <div className="bg-green-300 border-b-2 border-background divide-solid">
-        <h1 className="text-4xl lg:text-5xl lg:text-6xl xl:text-7xl xl:pt-28 lg:pt-22 lg:pt-24 font-bold text-center pt-10 font-cursive text-wheat">
+        <h1 className="text-4xl lg:text-5xl xl:text-7xl xl:pt-30 lg:pt-20 xl:pt-20 font-bold text-center pt-10 font-cursive text-wheat">
           DevSpaces
         </h1>
-        <p className="text-center text-xl lg:text-2xl lg:px-28 xl:px-32 text-white lg:py-12 py-6 px-10 font-play">Find live and scheduled Twitter spaces on tech, programming, and development</p>
+        <p className="text-center text-xl lg:text-2xl xl:text-3xl lg:px-16 xl:px-40 text-white lg:py-12 py-6 px-10 font-play">Find live and scheduled Twitter spaces on tech, programming, and development</p>
         <div className="flex justify-center">
-          <button className="bg-wheat font-play font-black text-lg px-24 py-4 my-5 rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-orange duration-200">Get Started</button>
+          <button className="bg-wheat font-play font-black text-lg xl:text-xl px-24 py-4 my-5 rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-orange duration-200">Get Started</button>
         </div>
       </div>
       <div className="max-w-full flex px-10 lg:hidden">
