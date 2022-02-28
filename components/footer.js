6156489cb0b597933b600e0b4efff08481fa8b1b
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-03-01 00:45:46 +0530

    footer padding corrected

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 function footer() {
     return (
-        <div className="font-black text-center bg-orange font-play py-2 w-109">
+        <div className="font-black text-center bg-orange font-play py-2">
             <p>
                 Made with ❤️ by Ashutosh Mishra
             </p>
