058ed3f34572dccf0e8836fa7dde795b1e49439d
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-02-28 20:33:06 +0530

    added metadata

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Header from '../components/header'
 import Hero from '../components/hero'
 import HowItWorks from '../components/working'
@@ -7,6 +8,11 @@ import Footer from '../components/footer'
 export default function Home({datas}) {
   return (
     <div className='bg-dark-background'>
+      <Head>
+        <title>DevSpaces</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="DevSpaces is a twitter spaces finder for programmers. It displays spaces on tech, programming, and development." />
+      </Head>
       {/* <Header /> */}
       <Hero />
       <HowItWorks />
