6e888278d75849647106cb9b0022fe955727f2ec
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-03-01 10:19:48 +0530

    footer link added

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ function footer() {
     return (
         <div className="font-black text-center bg-orange font-play py-2">
             <p>
-                Made with ❤️ by Ashutosh Mishra
+                Made with ❤️ by <a onClick={() => window.location.href = 'https:twitter.com/ashutoshmishrae'}>Ashutosh Mishra</a>
             </p>
             <p>
                 Build with NextJS. Deployed on Netlify
