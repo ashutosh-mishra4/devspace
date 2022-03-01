4229c944dfcb5e7f0c36f83553fc235ab4627970
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-03-01 11:03:00 +0530

    footer link bug fix

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ function footer() {
     return (
         <div className="font-black text-center bg-orange font-play py-2">
             <p>
-                Made with ❤️ by <a onClick={() => window.location.href = 'https:twitter.com/ashutoshmishrae'}>Ashutosh Mishra</a>
+                Made with ❤️ by <span className="hover:cursor-pointer" onClick={() => window.location.href = 'https:twitter.com/ashutoshmishrae'}>Ashutosh Mishra</span>
             </p>
             <p>
                 Build with NextJS. Deployed on Netlify
